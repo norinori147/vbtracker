a14adf9f5b4ad516d7facce87106229f19f7f7ce
norinori147 <caa41900@gmail.com>
2026-08-11 23:45:35 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'seimei-vb-tracker-v1-4-16b-set-history-label-padding-final-build-20260811';
+const CACHE_NAME = 'seimei-vb-tracker-v1-4-16c-set-summary-report-integration-build-20260811';
 const ASSETS = [
   './',
   './index.html',
